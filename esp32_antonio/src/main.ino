04b993f2b53f5fd9cde92da7b99af9c137f0d695
WannaJoin <43343012+WannaJoin@users.noreply.github.com>
2024-03-23 14:05:38 -0100

    ESP32_Antonio
    Moved the html code from the lambda to a multiline char

--- esp32_antonio/src/main.ino
+++ esp32_antonio/src/main.ino
@@ -8,6 +8,15 @@ const char* password = "30092016vm";
 // Pin connected to relay
 const int relayPin = 16;
 
+const char * htmlCode = R"(
+  <html>
+    <body>
+      <h1>ESP32 Relay Control</h1>
+      <p>Click<a href='/relay/on'>here</a> to turn the relay ON.</p>
+      <p>Click <a href='/relay/off'>here</a> to turn the relay OFF.</p>
+    </body>
+  </html>)";
+
 AsyncWebServer server(80);
 
 void setup() {
@@ -23,10 +32,9 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println("Connected to WiFi");
-
-
+  
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(200, "text/html", "<html><body><h1>ESP32 Relay Control</h1><p>Click <a href='/relay/on'>here</a> to turn the relay ON.</p><p>Click <a href='/relay/off'>here</a> to turn the relay OFF.</p></body></html>");
+    request->send(200, "text/html", htmlCode);
   });
 
   server.on("/relay/on", HTTP_GET, [](AsyncWebServerRequest *request){
