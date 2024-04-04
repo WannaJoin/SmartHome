2e98db9c52629c978bbee035a3538a37b1591732
Miguel Bilhete <miguelbilhete_2980@outlook.pt>
2024-04-04 19:44:56 +0000

    Commented the Websocket
    Added a few comments and reorganized the some of the code and removed some of the console.log.

--- mainNodeServer/app.js
+++ mainNodeServer/app.js
@@ -6,19 +6,36 @@ const wss = new WebSocket.Server({ port: 8080 });
 let jWaterTemp = [];
 loadDataFiles();
 
-wss.on('connection', function connection(ws) {
-  
+
+//        MAIN WEBSOCKET CODE
+
+wss.on('listening', () => {
+  const address = wss.address();
+  console.log(`WebSocket server is listening on ${address.address}:${address.port}`);
+});
+
+wss.on('connection', function connection(ws, req) {
+  //Log the IP of the connection for further verification
+  const deviceIp = req.socket.remoteAddress;
+  console.log(`Client connected from IP: ${deviceIp}`);
+
   ws.on('message', (message) => {    
+    // Handle the message received
     let parsedMessage = parseMessage(message);
 
     saveJWaterTemp(parsedMessage);
   });
+
 
   ws.on('close', function close() {
     console.log('Client disconnected');
   });
 });
 
+////////////////////
+
+
+//    HANDLERS AND OTHER FUNCTIONS
 
 function loadDataFiles(){
   //Load the temperature list to the global list
@@ -29,8 +46,8 @@ function loadDataFiles(){
   });
 }
   
-
 function parseMessage(message){
+  //Safely convert the message received and return it
   let parsedMessage;
   try {
     parsedMessage = JSON.parse(message);
@@ -42,12 +59,11 @@ function parseMessage(message){
 }
 
 function saveJWaterTemp(push){
-  
   //Push the value to the current list and log it.
   jWaterTemp.push(push);
-  console.log("New updated list: \n");
-  jWaterTemp.forEach((x) => console.log(x));
-  console.log("\n\n");
+  // console.log("New updated list: \n");
+  // jWaterTemp.forEach((x) => console.log(x));
+  // console.log("\n\n");
 
   fs.writeFile("data/jWaterTemp.json", JSON.stringify(jWaterTemp), (error) => {
     if (error) {
