bf8263e8c3b2a748652140d3aae4194b238513f2
Miguel Bilhete <miguelbilhete_2980@outlook.pt>
2024-04-04 19:22:53 +0000

    Webstocket v1 update
    Websocket working with a single sensor that sends date and value.
    -> next update: filter the action by IP.

--- mainNodeServer/app.js
+++ mainNodeServer/app.js
@@ -1,22 +1,58 @@
 const WebSocket = require('ws');
+const fs = require("fs");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
-const tempSaveArr = [];
+let jWaterTemp = [];
+loadDataFiles();
 
 wss.on('connection', function connection(ws) {
+  
+  ws.on('message', (message) => {    
+    let parsedMessage = parseMessage(message);
 
-  ws.on('message', function incoming(message) {    
-    let parsedMessage;
-    try {
-      parsedMessage = JSON.parse(message);
-    } catch (error) {
-      console.error('Error parsing JSON message:', error);
-      return;}
-      
+    saveJWaterTemp(parsedMessage);
   });
 
   ws.on('close', function close() {
     console.log('Client disconnected');
   });
 });
+
+
+function loadDataFiles(){
+  //Load the temperature list to the global list
+  fs.readFile("data/jWaterTemp.json", (error, data) => {
+    if (data != 0){
+      jWaterTemp = JSON.parse(data);
+    }
+  });
+}
+  
+
+function parseMessage(message){
+  let parsedMessage;
+  try {
+    parsedMessage = JSON.parse(message);
+  } catch (error) {
+    console.error('Error parsing JSON message:', error);
+    return;
+  }
+  return parsedMessage;
+}
+
+function saveJWaterTemp(push){
+  
+  //Push the value to the current list and log it.
+  jWaterTemp.push(push);
+  console.log("New updated list: \n");
+  jWaterTemp.forEach((x) => console.log(x));
+  console.log("\n\n");
+
+  fs.writeFile("data/jWaterTemp.json", JSON.stringify(jWaterTemp), (error) => {
+    if (error) {
+      console.error(error);
+      throw error;
+    }
+  });
+};
