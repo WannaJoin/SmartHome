2345051fd217b3f6519b96449f9c8dddc6351a9b
Miguel Bilhete <miguelbilhete_2980@outlook.pt>
2024-06-10 17:35:35 +0000

    Async Button
    Now the button runs Async and will toggle between on and off.
    Also the other Async task grabs the current hour and turns the light on and off, this can be overwritten by the button.

--- ESP32_Aquarium/src/main.ino
+++ ESP32_Aquarium/src/main.ino
@@ -4,9 +4,13 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <DHTesp.h>
+#include <ESP32Time.h>
 
 #define LIGHT_PIN 16
 #define HEAT_PIN 17 
+#define BUTTON_PIN 21
+#define DHT_PIN 16
+#define DHT_TYPE DHT11
 
 const char* ssid = "TP-3009";
 const char* password = "30092016vm";
@@ -21,62 +25,14 @@ WebsocketsClient wsClient;
 const int oneWireBus = 4; 
 OneWire oneWire(oneWireBus);
 DallasTemperature waterSensor(&oneWire);
-#define DHT_PIN 16
-#define DHT_TYPE DHT11
 DHTesp dht;
 /////////////////////////////
 
-
 bool wsConnected;
-
 bool lightState = false;
-bool heatState = false;
-
-
-
-void setup(){
-  Serial.begin(115200);
-  delay(100);
-
-  pinMode(LIGHT_PIN, OUTPUT);
-  pinMode(HEAT_PIN, OUTPUT);
-  dht.setup(DHT_PIN, DHTesp::DHT11);
-  
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi.");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println();
-
-  Serial.println("Connected to WiFi");
-  Serial.print("IP: ");
-  Serial. println(WiFi. localIP());
-  
-  wsConnect();
-  wsClient.onMessage([&](WebsocketsMessage message) {
-      Serial.print("Got Message: ");
-      Serial.println(message.data());
-      inMessageHandler(message.data());
-    }
-  );
-  delay(3000);
-}
-
-void loop(){  
-  Serial.print("Serial: ");
-  Serial.println(parsedData());
-
-  if(wsClient.available()) {
-    wsClient.poll();
-    wsClient.send(parsedData());
-  } else {
-    wsConnect();
-  }
-
-  delay(5000);
-}
+TaskHandle_t ButtonTask;
+TaskHandle_t ClockTask;
+ESP32Time rtc(0);
 
 String parsedData(){
   waterSensor.requestTemperatures();
@@ -96,29 +52,6 @@ String parsedData(){
 }
 
 void inMessageHandler(String message){
-    if (message == "l"){
-      if(lightState){
-        lightState = false;
-        digitalWrite(LIGHT_PIN, LOW);
-        wsClient.send("loff");
-      } else {
-        lightState = true;
-        digitalWrite(LIGHT_PIN, HIGH);
-        wsClient.send("lon");
-      }
-    }
-
-    if (message == "h"){
-      if(heatState){
-        heatState = false;
-        digitalWrite(HEAT_PIN, LOW);
-        wsClient.send("hoff");
-      } else {
-        heatState = true;
-        digitalWrite(HEAT_PIN, HIGH);
-        wsClient.send("hon");
-      }
-    }
 }
 
 void wsConnect(){
@@ -129,4 +62,88 @@ void wsConnect(){
   } else {
     Serial.println("Not Connected!");
   }
+}
+
+void checkButton(void *parameter){
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  
+  while(true){
+    if(digitalRead(BUTTON_PIN) == LOW){
+      Serial.println("Button was pressed!");
+      if(lightState) lightState = false;
+      else lightState = true;
+      vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+  }
+}
+
+void checkHour(void *parameter){
+  while (true) {
+    int currentHour = rtc.getHour(true);
+    Serial.print("Current Hour: ");
+    Serial.println(currentHour);
+
+    if (currentHour >= 8 && currentHour <= 21) {
+      Serial.println("8AM: Turn Ligth On");
+      lightState = true;
+    } else {
+      Serial.println("9PM: Turn Light Off");
+      lightState = false;
+    }
+    vTaskDelay(15000 / portTICK_PERIOD_MS);
+  }
+}
+
+
+void setup(){
+  xTaskCreate(checkButton, "checkButton", 10000, NULL, 1, &ButtonTask);
+  xTaskCreate(checkHour, "checkHour", 10000, NULL, 1, &ClockTask);
+
+  Serial.begin(115200);
+  delay(100);
+
+  pinMode(LIGHT_PIN, OUTPUT);
+  pinMode(HEAT_PIN, OUTPUT);
+  dht.setup(DHT_PIN, DHTesp::DHT11);
+  
+  // WiFi.begin(ssid, password);
+  // Serial.print("Connecting to WiFi.");
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(1000);
+  //   Serial.print(".");
+  // }
+  Serial.println();
+
+  Serial.println("Connected to WiFi");
+  Serial.print("IP: ");
+  Serial. println(WiFi. localIP());
+  
+  // wsConnect();
+  // wsClient.onMessage([&](WebsocketsMessage message) {
+  //     Serial.print("Got Message: ");
+  //     Serial.println(message.data());
+  //     inMessageHandler(message.data());
+  //   }
+  // );
+  delay(3000);
+}
+
+void loop(){  
+  //Serial.println("Serial: ");
+  //Serial.println(parsedData());
+
+  // if(wsClient.available()) {
+  //   wsClient.poll();
+  //   wsClient.send(parsedData());
+  // } else {
+  //   wsConnect();
+  // }
+
+  if (lightState){
+    digitalWrite(LIGHT_PIN, HIGH);
+  } else {
+    digitalWrite(LIGHT_PIN, LOW);
+  }
+  delay(500);
 }
